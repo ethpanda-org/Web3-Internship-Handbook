17b625a25b3080bf97c8e03d4972756c4cc43968
“EchoZheng2333” <1137077228@qq.com>
2025-06-20 17:04:36 +0800

    feat: 更新手册内容与结构优化
    本次提交主要包括以下更新事项：
    
    1. 内容更新与优化
       - 修改和完善了首页、导航栏等核心文档内容，使表达更贴合目标用户（高校学生、Web3 实习生）。
       - 增加/调整了“为什么需要这本手册”、“如何参与”、“生态伙伴计划”等板块，提升了内容逻辑和可读性。
       - 修正了部分错别字、表达不清等问题。
    
    2. 图片与资源路径调整
       - 统一整理了图片资源路径，确保所有图片引用正确，解决了构建报错问题。
       - 删除了冗余或无用的图片和文档资源。
    
    3. 目录结构优化
       - 优化了 docs/handbook 及其子目录结构，提升了项目的可维护性。
       - 清理了不再使用的 demo、预览等目录。
    
    4. 配置文件更新
       - 更新了 .gitignore，忽略了 promt* 等本地辅助文件。
       - 优化了 docs/.vuepress/navbar.ts，补充了导航栏条目，指向最新内容。
    
    5. 其他修复
       - 删除了部分重复、无效或过时的 Markdown 文件。
       - 解决了合并冲突，保证主分支内容一致且可用。

--- docs/.vuepress/.temp/pages/index.html.vue
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -1,61 +1,25 @@
 <template><div><!-- 这里是自定义区域的内容，会插入到 type: custom 区域的位置 -->
-<h1 id="如何参与" tabindex="-1"><a class="header-anchor" href="#如何参与"><span>如何参与</span></a></h1>
-<!-- :::: card-masonry
-
-
-  ::: card title="Alex"
-  ![Alex](./handbook/images/logo/640.png)
-  Web3 实习手册制作人
-  :::
-
-  ::: card title="Luna"
-  ![Luna](./handbook/images/logo/640.png)
-  Web3 实习手册制作人
-  :::
-
-  ::: card title="Jason"
-  ![Jason](./handbook/images/logo/640.png)
-  Web3 实习手册制作人
-  :::
-:::: -->
+<h1 id="更多信息" tabindex="-1"><a class="header-anchor" href="#更多信息"><span>更多信息</span></a></h1>
 <!-- 贡献者样式
 ![GitHub contributors](https://img.shields.io/github/contributors/pengzhanbo/vuepress-theme-plume?color=32A9C3&labelColor=1B3C4A&logo=contributorcovenant)
 
 [![contributors](https://contrib.rocks/image?repo=pengzhanbo/vuepress-theme-plume)](https://github.com/ethpanda-org/Web3-Internship-Handbook/graphs/contributors)
 
 ![Repo beats](https://repobeats.axiom.co/api/embed/b3e16e8802010e72e933f64864a8047e3163e927.svg "Repo Beats analytics image") -->
-<h2 id="web3-练习生计划" tabindex="-1"><a class="header-anchor" href="#web3-练习生计划"><span>Web3 练习生计划</span></a></h2>
-<h2 id="icon-src-handbook-images-home-brainstorming-svgwidth-45pxheight-45pxdetails-我们会在实习季开始前启动为期-3-个月的练习生计划-围绕手册内容进行共学、答疑和模拟面试-并为你对接真实的实习机会。link-web3-handbook-part2-position-introduction-mdlinktext-了解详情" tabindex="-1"><a class="header-anchor" href="#icon-src-handbook-images-home-brainstorming-svgwidth-45pxheight-45pxdetails-我们会在实习季开始前启动为期-3-个月的练习生计划-围绕手册内容进行共学、答疑和模拟面试-并为你对接真实的实习机会。link-web3-handbook-part2-position-introduction-mdlinktext-了解详情"><span>icon:
-src: ./handbook/images/home/brainstorming.svg
-width: 45px
-height: 45px
-details: 我们会在实习季开始前启动为期 3 个月的练习生计划，围绕手册内容进行共学、答疑和模拟面试，并为你对接真实的实习机会。
-link: ./web3-handbook/part2/position-introduction.md
-linkText: 了解详情</span></a></h2>
-<h2 id="社群入口" tabindex="-1"><a class="header-anchor" href="#社群入口"><span>社群入口</span></a></h2>
-<h2 id="icon-src-handbook-images-community-chatgpt-image-2025年6月2日-15-02-01-pngwidth-45pxheight-45pxdetails-加入我们的开发者和学习者社群-共同探讨-web3-技术-分享学习心得。link-https-lxdao-io-linktext-加入我们" tabindex="-1"><a class="header-anchor" href="#icon-src-handbook-images-community-chatgpt-image-2025年6月2日-15-02-01-pngwidth-45pxheight-45pxdetails-加入我们的开发者和学习者社群-共同探讨-web3-技术-分享学习心得。link-https-lxdao-io-linktext-加入我们"><span>icon:
-src: ./handbook/images/community/ChatGPT_Image_2025年6月2日_15_02_01.png
-width: 45px
-height: 45px
-details: 加入我们的开发者和学习者社群，共同探讨 Web3 技术，分享学习心得。
-link: https://lxdao.io/
-linkText: 加入我们</span></a></h2>
-<h2 id="title-成为赞助方icon-src-handbook-images-logo-lxdao-pngwidth-45pxheight-45pxdetails-你的赞助将帮助我们持续创作高质量内容-支持-web3-教育和开源事业。link-https-lxdao-io-linktext-联系我们" tabindex="-1"><a class="header-anchor" href="#title-成为赞助方icon-src-handbook-images-logo-lxdao-pngwidth-45pxheight-45pxdetails-你的赞助将帮助我们持续创作高质量内容-支持-web3-教育和开源事业。link-https-lxdao-io-linktext-联系我们"><span>title: 成为赞助方
-icon:
-src: ./handbook/images/logo/LXDAO.png
-width: 45px
-height: 45px
-details: 你的赞助将帮助我们持续创作高质量内容，支持 Web3 教育和开源事业。
-link: https://lxdao.io/
-linkText: 联系我们</span></a></h2>
-<p>title: 支持社区
-icon:
-src: ./handbook/images/logo/640.png
-width: 45px
-height: 45px
-details: 感谢所有为本项目提供支持的社区伙伴，你们是生态繁荣的基石。
-link: https://ethpanda.org
-linkText: 查看伙伴</p>
-</div></template>
+<br>
+<VPCard><h2 id="web3-练习生计划" tabindex="-1"><a class="header-anchor" href="#web3-练习生计划"><span><VPIcon provider="iconify" name="emojione-v1:ledger" /> Web3 练习生计划</span></a></h2>
+<p>我们会在实习季开始前启动为期 <mark>3 个月</mark>的练习生计划，围绕手册内容进行共学、答疑和模拟面试，并为你对接真实的实习机会。</p>
+<div style="text-align:right"><p><a href="/" target="_blank" rel="noopener noreferrer">了解详情</a></p>
+</div></VPCard><VPCard><h2 id="生态伙伴计划" tabindex="-1"><a class="header-anchor" href="#生态伙伴计划"><span><VPIcon provider="iconify" name="emojione-v1:left-speech-bubble" /> 生态伙伴计划</span></a></h2>
+<p><mark class="info">共建 Web3 人才生态</mark></p>
+<p>我们欢迎各类 Web3 项目方、社区、媒体与我们合作，共同建设实习生生态。如果你希望参与我们的实习生计划，为行业培养人才，请联系我们。</p>
+<div style="text-align:right"><p><a href="/" target="_blank" rel="noopener noreferrer">联系我们</a></p>
+</div></VPCard><VPCard><h2 id="社群入口" tabindex="-1"><a class="header-anchor" href="#社群入口"><span><VPIcon provider="iconify" name="emojione-v1:busts-in-silhouette" /> 社群入口</span></a></h2>
+<p>加入我们的开发者和学习者社群，共同探讨 Web3 技术，分享学习心得。</p>
+<div style="text-align:right"><p><a href="/" target="_blank" rel="noopener noreferrer">加入我们</a></p>
+</div></VPCard><VPCard><h2 id="支持社区" tabindex="-1"><a class="header-anchor" href="#支持社区"><span><VPIcon provider="iconify" name="emojione-v1:white-heavy-check-mark" /> 支持社区</span></a></h2>
+<p>感谢所有为本项目提供支持的社区伙伴，你们是生态繁荣的基石。</p>
+<div style="text-align:right"><p><a href="/" target="_blank" rel="noopener noreferrer">查看伙伴</a></p>
+</div></VPCard></div></template>
 
 
